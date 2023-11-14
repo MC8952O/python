d475073270780ed11568f22e8cf25e0628d4ce96
MC8952O <13050006282@139.com>
2023-11-14 23:56:43 +0800

    高级数据类型-元组学习
    高级数据类型-元组学习

--- pythonStudy/debug.py
+++ pythonStudy/debug.py
@@ -1,4 +1,14 @@
-sliceStr = 'hello mia good good study'
-#获取0~5之间的字符
-print(sliceStr[:])
-print(sliceStr[0])
+myTuple = ()
+print(type(myTuple)) #<class 'tuple'>
+myTuple = (1, 2, 'hello python', 23.4, (1, 3, 'mia'), [12, 'mia', 12.4, (1,)], True)
+print(myTuple)
+#遍历元组
+for i in myTuple:
+    print(i, end =" ")
+print()
+print(myTuple[::-2])
+print(myTuple[-5:-1:1]) #
+print(myTuple[-4:-2:-1])
+myTuple[5][1] = 'mia1'
+print(myTuple)
+print(myTuple.count(1)) #2
--- pythonStudy/seniorDataType3.py
+++ pythonStudy/seniorDataType3.py
@@ -20,7 +20,32 @@
     2.8 replace(old, new, coount = None) 将字符串中的指定字符串替换为指定的字符串 old:被替换的字符串 new:替换的字符串 count:替换的次数,无表示全部替换
     2.9 count() 统计字符串中指定字符串出现的次数
 2 列表及常用方法
+    2.1 定义：是有序的数据集合
+    2.2 特点：
+        2.2.1 支持增删改查
+        2.2.2 列表中的数据是可以变化的【数据项可以变化，但是内存地址不会改变】
+        2.2.3 使用[]表示列表，数据项之间使用“，”分割，数据项可以是任何类型的数据
+        2.2.4 支持索引和切片的操作处理
+    2.3 常用的方法
+        2.3.1 append() 在列表的末尾添加新的元素 语法：list.append(obj)
+        2.3.2 count() 统计某个元素在列表中出现的次数 语法：list.count(obj)
+        2.3.3 extend() 在列表的末尾一次性追加另一个序列中的多个值 语法：list.extend(seq)
+        2.3.4 index() 从列表中找出某个值第一个匹配项的索引位置 语法：list.index(obj)
+        2.3.5 insert() 将对象插入列表 语法：list.insert(index, obj)
+        2.3.6 pop() 移除列表中的一个元素（默认最后一个元素），并且返回该元素的值 语法：list.pop(index = -1)
+        2.3.7 remove() 移除列表中某个值的第一个匹配项 语法：list.remove(obj)
+        2.3.8 reverse() 反向列表中元素 语法：list.reverse()
+        2.3.9 sort() 对原列表进行排序 语法：list.sort(cmp = None, key = None, reverse = False)
+        2.3.2 clear() 清空列表 语法：list.clear() 
 3 元组及常用方法
+    3.1 定义：是一种不可变的序列，在创建之后不能够做任何修改 使用()创建元组
+    3.2 特点：元组类型可以是任何数据类型
+              当元组只有一个元素时，需要在元素后面添加逗号，不然解释器会当作整型处理
+              支持切片
+    3.3常用方法：
+        3.3.1 count() 统计某个元素在元组中出现的次数 语法：tuple.count(obj)
+        3.3.2 index() 从元组中找出某个值第一个匹配项的索引位置 语法：tuple.index(obj)
+                
 4 字典及常用方法
 5 共有操作
 6 序列(集合)
@@ -98,3 +123,95 @@ print(sliceStr[:])
 print(sliceStr[:3])
 #倒序输出 负号
 print(sliceStr[::-1])
+#列表定义
+myList = [1, 2, 3, 'hello python', 12.3 , True]
+#查看序列长度(针对与字符串)，查看序列中元素的个数(针对于列表)
+print(len(myList)) #4
+myStr = 'my name is mia'
+print(len(myStr)) #14
+#查找
+# 输出第一个元素
+print(myList[0]) #1
+#输出2-3个元素
+print(myList[1:3]) #2,3
+#输出第3个开始的所有
+print(myList[2:]) #3,'hello python',12.3,True
+#倒叙输出
+print(myList[::-1]) #True,12.3,'hello python',3,2,1
+# 列表计算 复制列表
+print(myList*2) #[1, 2, 3, 'hello python', 12.3, True, 1, 2, 3, 'hello python', 12.3, True]
+# 列表增加 常用的函数 append() insert(index, obj) extend()
+# 追加元素追加的元素数据类型不做限制
+myList.append('mia.chen')
+print(myList) #[1, 2, 3, 'hello python', 12.3, True, 'mia.chen']
+myList.append([1, 2, 3])
+print(myList) #[1, 2, 3, 'hello python', 12.3, True, 'mia.chen', [1, 2, 3]]
+# 插入元素  insert(index, obj)
+myList.insert(4, '在4的位置插入改元素')
+print(myList) #[1, 2, 3, 'hello python', '在4的位置插入改元素', 12.3, True, 'mia.chen', [1, 2, 3]]
+myList.insert(12.3)
+#批量插入  extend() #追加的元素必须是序列
+rsData = list(range(10)) #强制转换为列表
+print(type(rsData)) #<class 'list'>
+myList.extend(rsData)
+print(myList) #[1, 2, 3, 'hello python', '在4的位置插入改元素', 12.3, True, 'mia.chen', [1, 2, 3], 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#修改元素
+updateList = [1, 2, 3, 'hello python', 12.3 , True]
+#根据索引修改
+updateList[2] = '将第二个元素修改'
+print(updateList) #[1, 2, '将第二个元素修改', 'hello python', 12.3, True]
+# 修改---删除元素
+listDelete = list(range(10, 50))
+#删除第一项元素
+del listDelete[0]
+print(listDelete)
+#批量删除
+del listDelete[1:3]
+print(listDelete) #[11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22...]
+#remove() 删除指定的元素
+listDelete.remove(22) #删除22
+print(listDelete) #[11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23...]
+# pop(index)删除指定索引的元素
+listDelete.pop(0)
+#查找元素的索引
+print(listDelete.index(24)) #6
+#元素反转
+listDelete.reverse()
+print(listDelete) #[49, 48, 47, 46, 45, 44, 43, 42, 41, 40...]
+#统计列表中元素个数
+print(listDelete.count(23)) #0
+#列表排序
+listSort = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#正序
+listSort.sort()
+print(listSort) #[1, 2, 3, 4, 5, 6, 7, 8, 9]
+#倒序
+listSort.sort(reverse = True)
+print(listSort) #[9, 8, 7, 6, 5, 4, 3, 2, 1]
+#元组
+#定义一个元组
+myTuple = ()
+print(type(myTuple)) #<class 'tuple'>
+myTuple = (1, 2, 'hello python', 23.4, (1, 3, 'mia'), [12, 'mia', 12.4, (1,)], True)
+print(myTuple)
+#只能对元组进行查询操作
+#遍历元组
+for i in myTuple:
+    print(i, end =" ") #1 2 hello python 23.4 (1, 3, 'mia') [12, 'mia', 12.4, (1,)] True
+#通过下边取值
+print(myTuple[1]) #2
+#切片 4~5的元素 
+print(myTuple[3:5]) #23.4 (1, 3, 'mia')
+# 切片倒序
+print(myTuple[::-1]) #True [12, 'mia', 12.4, (1,)] (1, 3, 'mia') 23.4 hello python 2 1
+#倒序切片1
+print(myTuple[::-2]) #(True, (1, 3, 'mia'), 'hello python', 1)
+#倒序切片2
+print(myTuple[-5:-1:1]) #('hello python', 23.4, (1, 3, 'mia'), [12, 'mia', 12.4, (1,)])
+#倒序切片3
+print(myTuple[-4:-2:-1]) #()
+#可以对元组中的列表进行修改
+myTuple[6][1] = 'mia'
+print(myTuple)
+#统计元组中某个元素出现的次数
+print(myTuple.count(1)) #2
