ef62dcae6acd7d723f416df4a7510c5a6d9bfebb
MC8952O <13050006282@139.com>
2023-11-15 23:49:09 +0800

    练习题

---
+++ pythonStudy/dataTypePractic.py
@@ -0,0 +1,40 @@
+'''
+编写一个程序将分钟转换为秒。
+'''
+'''inputMiunte = int(input('请输入需要转换的时间：'))
+print('{}分钟转换后的时间为：{}s'.format(inputMiunte, inputMiunte*60))'''
+'''
+编写一个程序将字符串转换为整数
+'''
+'''inputStr = input('请输入一个字符串：')
+print('{}转换后的整数为：{}'.format(inputStr, int(inputStr)),type(int(inputStr)))'''
+'''eg = [1, 4 ,-10 ,-50 ,32 ,21]
+print(len(eg))
+a = 0
+for i in range(len(eg)-1):
+    for j in range(i+1,len(eg)):
+        if eg[i] > eg[j]:
+            a = eg[j]
+            eg[j] = eg[i]
+            eg[i] = a
+print(eg[0])
+print(eg[5])
+print(eg[5] - eg[0])'''
+'''eg = [1, 2 ,3]
+print(eg[len(eg)-1])
+print(type(eg[len(eg)-1]))'''
+'''strA = 'ABC'
+StrB = 'CDFE'
+for i in strA:
+    for j in StrB:
+        if i == j:
+            print(True)'''
+#字符串首尾连接
+strA = 'love pythontip'
+
+print(strA[0:1]+strA[len(strA)-1])
+
+            
+
+
+
